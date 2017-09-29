f1171a3789ff754daae91f5c7b7d400f53602c3a
DaniMangtani <DaniMangtani@10.230.105.165>
2017-09-29 15:48:53 +0100

    Cuarta version de Kata2

--- src/kataa2/Kataa2.java
+++ src/kataa2/Kataa2.java
@@ -2,16 +2,14 @@ package kataa2;
 
 import java.util.*;
 
-public class Kataa2{
-    public static void main(String[] args) {
-        int [] data = {1,1,1,0,1,2,1,0,3};
-        Map<Integer, Integer> myMap = new HashMap<>();
-        
+public class Kataa2 {
+    public static void main(String[] args) { 
+        String [] data = {"uno", "uno", "dos", "tres", "tres"};
         Histogram histo = new Histogram(data);
-        Map<Integer, Integer> histogr = histo.getHistogram();
+        Map<String,Integer> histogr = histo.getHistogram();
         
-        for (int key : histogr.keySet()){
+        for (String key : histogr.keySet()){
             System.out.println(key + " ==> " + histogr.get(key) + " veces");
         }
-    }  
+    }
 }

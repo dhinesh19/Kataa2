3b5fa4f6bcfb8962d96b5c6f5918b04f3002e099
DaniMangtani <DaniMangtani@10.230.105.165>
2017-09-29 15:31:50 +0100

    Segunda version de Kata2

--- src/kataa2/Kataa2.java
+++ src/kataa2/Kataa2.java
@@ -7,16 +7,13 @@ public class Kataa2{
         int [] data = {1,1,1,0,1,2,1,0,3};
         Map<Integer, Integer> myMap = new HashMap<>();
         
-        for (int i = 0; i < data.length; i++) {
-            if(myMap.containsKey(data[i])){
-                myMap.put(data[i], myMap.get(data[i]) + 1);
-            } else {
-                myMap.put(data[i], 1);
-            }
+        for (int key : data) {
+            myMap.put(key, myMap.containsKey(key) ?
+            myMap.get(key) + 1 : 1);
         }
-       
+        
         for (int key : myMap.keySet()){
             System.out.println(key + " ==> " + myMap.get(key) + " veces");
         }
     }  
- }
+}

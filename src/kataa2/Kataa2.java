63990c3dc008f5bf8db27ac2aead38860c196cd1
DaniMangtani <DaniMangtani@10.230.105.165>
2017-09-29 15:25:27 +0100

    Primerea version Kata2

---
+++ src/kataa2/Kataa2.java
@@ -0,0 +1,22 @@
+package kataa2;
+
+import java.util.*;
+
+public class Kataa2{
+    public static void main(String[] args) {
+        int [] data = {1,1,1,0,1,2,1,0,3};
+        Map<Integer, Integer> myMap = new HashMap<>();
+        
+        for (int i = 0; i < data.length; i++) {
+            if(myMap.containsKey(data[i])){
+                myMap.put(data[i], myMap.get(data[i]) + 1);
+            } else {
+                myMap.put(data[i], 1);
+            }
+        }
+        
+        for (int key : myMap.keySet()){
+            System.out.println(key + " ==> " + myMap.get(key) + " veces");
+        }
+    }  
+ }
